cfed8490148147643ec1f432c96f73195bd24512
Manomenu <88382777+Manomenu@users.noreply.github.com>
2023-07-12 19:21:00 +0200

    more-cleaning

--- parafia-mbkm/ClientApp/src/app/app.module.ts
+++ parafia-mbkm/ClientApp/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ContactComponent } from './contact/contact.component';
 
@@ -17,8 +14,6 @@ import { ContactComponent } from './contact/contact.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ArticlesComponent,
     ContactComponent,
   ],
@@ -29,8 +24,6 @@ import { ContactComponent } from './contact/contact.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'api/article', component: ArticlesComponent },
       { path: 'api/contact', component: ContactComponent },
     ]),
