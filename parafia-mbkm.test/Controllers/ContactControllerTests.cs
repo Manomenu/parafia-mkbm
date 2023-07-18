6c7769df291c6fcb8067cb9e253fb04ee74a43d3
Manomenu <88382777+Manomenu@users.noreply.github.com>
2023-07-18 23:49:41 +0200

    basic-contact-controller-unit-tests

--- parafia-mbkm.test/Controllers/ContactControllerTests.cs
+++ parafia-mbkm.test/Controllers/ContactControllerTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using parafia_mbkm.Controllers;
 using parafia_mbkm.data.Models;
+using parafia_mbkm.Models;
 using parafia_mbkm.Services.IServices;
 
 namespace parafia_mbkm.test.Services
@@ -32,6 +33,107 @@ namespace parafia_mbkm.test.Services
             ///Assert
             ///
             result.Result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public void ContactController_GetAllContacts_ReturnNotFound()
+        {
+            ///Arrange
+            ///
+            var contacts = A.Fake<List<Contact>>();
+            contacts = null;
+            A.CallTo(() => _contactService.GetAllContactsAsync())
+                .Returns(contacts);
+            var controller = new ContactController(_contactService);
+
+            ///Act
+            ///
+            var result = controller.GetAllContacts();
+
+            ///Assert
+            ///
+            result.Result.Should().BeOfType(typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public void ContactController_GetContactById_ReturnBadRequest()
+        {
+            ///Arrange
+            ///
+            var id = -1;
+            var contact = A.Fake<Contact>();
+            contact = null;
+            A.CallTo(() => _contactService.GetContactByIdAsync(id))
+                .Returns(contact);
+            var controller = new ContactController(_contactService);
+
+            ///Act
+            ///
+            var result = controller.GetContactById(id);
+
+            ///Assert
+            ///
+            result.Result.Should().BeOfType(typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public void ContactController_GetContactById_ReturnOk()
+        {
+            ///Arrange
+            ///
+            var id = 1;
+            var contact = A.Fake<Contact>();
+            A.CallTo(() => _contactService.GetContactByIdAsync(id))
+                .Returns(contact);
+            var controller = new ContactController(_contactService);
+
+            ///Act
+            ///
+            var result = controller.GetContactById(id);
+
+            ///Assert
+            ///
+            result.Result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public void ContactController_AddContact_ReturnBadRequest()
+        {
+            ///Arrange
+            ///
+            var contact = A.Fake<ContactModel>();
+            var contactId = -1;
+            A.CallTo(() => _contactService.AddContactAsync(contact))
+                .Returns(contactId);
+            var controller = new ContactController(_contactService);
+
+            ///Act
+            ///
+            var result = controller.AddContact(contact);
+
+            ///Assert
+            ///
+            result.Result.Should().BeOfType(typeof(BadRequestResult));
+        }
+
+        [Fact]
+        public void ContactController_AddContact_ReturnCreatedAtAction()
+        {
+            ///Arrange
+            ///
+            var contact = A.Fake<ContactModel>();
+            var contactId = 1;
+            A.CallTo(() => _contactService.AddContactAsync(contact))
+                .Returns(contactId);
+            var controller = new ContactController(_contactService);
+
+            ///Act
+            ///
+            var result = controller.AddContact(contact);
+
+            ///Assert
+            ///
+            result.Result.Should().BeOfType(typeof(CreatedAtActionResult));
         }
     }
 }
