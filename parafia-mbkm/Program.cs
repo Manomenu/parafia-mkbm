6ba9cc7fc06c006a1e4d499954e6b0eea1627848
Manomenu <88382777+Manomenu@users.noreply.github.com>
2023-07-19 22:19:30 +0200

    author-authen-init

--- parafia-mbkm/Program.cs
+++ parafia-mbkm/Program.cs
@@ -1,12 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using parafia_mbkm;
 using parafia_mbkm.data;
 using parafia_mbkm.Services;
 using parafia_mbkm.Services.IServices;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddAuthentication(o => 
+{
+    o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ParafiaDbDataContext>(
     o => o.UseNpgsql(builder.Configuration.GetConnectionString("ParafiaDb"),
@@ -16,19 +40,25 @@ builder.Services.AddScoped<IContactService, ContactService>();
 
 var app = builder.Build();
 
+///Order of those middleware command lines below actually matters
+///<middleware>
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
-app.Seed();
 app.MapFallbackToFile("index.html");
+
+///</middleware>
+
+app.Seed();
 
 app.Run();
